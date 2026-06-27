d28cb954a3749ed99c016ba8239330c5bdf7dfde
jesus roque <2530078@upv.edu.mx>
2026-06-27 00:43:25 -0600

    Corregir puerto para Railway

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// Iniciar servidor
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
