cc761510b05ec6d1e478fcd0d0c3bd2a80b9ee53
jesus roque <2530078@upv.edu.mx>
2026-06-29 11:43:30 -0600

    Modificacion de db.js

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const pool = require('./src/config/db');
 
 const PORT = process.env.PORT || 3000;
 
-// SOLO LOG (no bloquear arranque)
-pool.getConnection()
-    .then(conn => {
-        console.log("✅ MySQL conectado correctamente");
-        conn.release();
-    })
-    .catch(err => {
-        console.error("❌ Error MySQL:", err.message);
-    });
-
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
+    
+    pool.getConnection()
+        .then(conn => {
+            console.log("✅ MySQL conectado correctamente");
+            conn.release();
+        })
+        .catch(err => {
+            console.error("❌ Error MySQL:", err.message);
+            // No cerramos el servidor
+        });
 });
